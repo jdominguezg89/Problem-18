e3fdb467a5c826982c0b5c356230f8be33916d16
jdominguezg89 <jdominguezg1989@gmail.com>
2017-01-26 23:35:45 -0500

    Add files via upload

---
+++ TDDProyect/TDDClassTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SumaRutaValorMaximoNameSpace;
+
+namespace TDDProyectNameSpace
+{
+    [TestClass]
+    public class TDDClassTester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SumaRutaValorMaximoClass sumaValorMaximo = new SumaRutaValorMaximoClass();
+            int sumaMaxima = sumaValorMaximo.SumaRutaValorMaximoAction();
+
+            //Este es el valor que se espera que devuelva el argoritmo 1074
+            Assert.AreEqual(7273, sumaMaxima);
+        }
+    }
+}
