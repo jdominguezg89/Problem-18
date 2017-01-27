bed3a3e1a4ad6c090c99f10065bbacaf9af31670
jdominguezg89 <jdominguezg1989@gmail.com>
2017-01-26 23:32:32 -0500

    Add files via upload

---
+++ Ejercicio18/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Ejercicio18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRutaFinalTomada = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSumaMaxima = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCantidadTotalRutas = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(5, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(294, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Buscar txt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 120);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(705, 305);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.richTextBox2);
+            this.panel1.Controls.Add(this.txtCantidadTotalRutas);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtSumaMaxima);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtRutaFinalTomada);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(705, 120);
+            this.panel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(386, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Calcular suma de la ruta máxima";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ruta final tomada:";
+            // 
+            // txtRutaFinalTomada
+            // 
+            this.txtRutaFinalTomada.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRutaFinalTomada.Location = new System.Drawing.Point(121, 33);
+            this.txtRutaFinalTomada.Name = "txtRutaFinalTomada";
+            this.txtRutaFinalTomada.Size = new System.Drawing.Size(571, 20);
+            this.txtRutaFinalTomada.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Suma máxima:";
+            // 
+            // txtSumaMaxima
+            // 
+            this.txtSumaMaxima.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSumaMaxima.Location = new System.Drawing.Point(121, 60);
+            this.txtSumaMaxima.Name = "txtSumaMaxima";
+            this.txtSumaMaxima.Size = new System.Drawing.Size(123, 20);
+            this.txtSumaMaxima.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cantidad total de rutas:";
+            // 
+            // txtCantidadTotalRutas
+            // 
+            this.txtCantidadTotalRutas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCantidadTotalRutas.Location = new System.Drawing.Point(121, 87);
+            this.txtCantidadTotalRutas.Name = "txtCantidadTotalRutas";
+            this.txtCantidadTotalRutas.Size = new System.Drawing.Size(123, 20);
+            this.txtCantidadTotalRutas.TabIndex = 8;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(250, 59);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(442, 48);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 425);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Cálculo de la suma de la ruta máxima";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtSumaMaxima;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRutaFinalTomada;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCantidadTotalRutas;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Ejercicio18/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio18
+{
+    public partial class Form1 : Form
+    {
+        private string[] numerosLineas;
+        private int cantidadLineas;
+        int[][] matrizValores;
+        int[][] copiaMatrizOriginal;
+        public Form1()
+        {
+            InitializeComponent();
+            cantidadLineas = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            txtRutaFinalTomada.Text = string.Empty;
+            txtSumaMaxima.Text = string.Empty;
+            txtCantidadTotalRutas.Text = string.Empty;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    textBox1.Text = openFileDialog1.FileName;
+                    string nuevaLinea = string.Empty;
+
+                    System.IO.StreamReader contenidoArchivo = new System.IO.StreamReader(openFileDialog1.FileName);
+
+                    //Se cuenta la cantidad de lineas que tiene el archivo para establecer la dimension del arreglo
+                    while ((nuevaLinea = contenidoArchivo.ReadLine()) != null)
+                        cantidadLineas++;
+                    contenidoArchivo.BaseStream.Seek(0, SeekOrigin.Begin);
+
+                    //Establesco el tamaño de la matriz de los valores
+                    //================================================
+                    matrizValores = new int[cantidadLineas][];
+                    copiaMatrizOriginal = new int[cantidadLineas][];
+                    for (int i = 0; i < cantidadLineas; i++)
+                    {
+                        matrizValores[i] = new int[cantidadLineas];
+                        copiaMatrizOriginal[i] = new int[cantidadLineas];
+                    }
+                    //================================================              
+
+                    int x = 0;
+                    while ((nuevaLinea = contenidoArchivo.ReadLine()) != null)
+                    {
+                        numerosLineas = nuevaLinea.Split(' ');
+                        for (int y = 0; y < numerosLineas.Length; y++)
+                        {
+                            matrizValores[x][y] = int.Parse(numerosLineas[y]);
+                            copiaMatrizOriginal[x][y] = int.Parse(numerosLineas[y]);
+                        }
+                        x++;
+                    }
+                    contenidoArchivo.Close();
+
+                    //Dibujar la matriz en pantalla
+                    string lineaMatriz = string.Empty;
+                    StringBuilder sbLineaMatriz = new StringBuilder();
+                    int contadosEspacios = matrizValores.Length * 2 - 2;
+                    for (int i = 0; i < matrizValores.Length; i++)
+                    {
+                        lineaMatriz = string.Empty;
+
+                        for (int j = 0; j < matrizValores[i].Length; j++)
+                        {
+                            if (matrizValores[i][j] != 0)
+                            {
+                                if (matrizValores[i][j] < 10)
+                                {
+                                    lineaMatriz += "0" + matrizValores[i][j].ToString() + " ";
+                                }
+                                else
+                                {
+                                    lineaMatriz += matrizValores[i][j].ToString() + " ";
+                                }
+
+                            }
+                            else
+                            {
+                                //lineaMatriz += "00 ";
+                            }
+                        }
+                        sbLineaMatriz.AppendLine(string.Empty.PadLeft(contadosEspacios, ' ') + lineaMatriz);
+                        contadosEspacios -= 2;
+                    }
+
+                    richTextBox1.Text = sbLineaMatriz.ToString();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[][] matrizPiramidal = this.matrizValores;
+            List<string> listaActualTomada = new List<string>();
+            List<string> copiaListaActualTomada = new List<string>();
+            List<string> listaRutaFinalTomada = new List<string>();
+            StringBuilder historialRutas = new StringBuilder();
+
+            //Se establece la cantidad total de rutas posibles dada la dimención de la matriz.
+            //Que es como bien se explica en el problema 67 2 elevado a la cantidad de filas de la piramide.
+            long cantidadTotalRutasPosibles = (long)Math.Pow(2, matrizPiramidal.Length - 1);
+
+            //Solución rapida para cuando es uan piramide no muy grande
+            if (cantidadTotalRutasPosibles > 0)
+            {
+                int sumaRutaMaxima = 0;
+                int sumaActual;
+                //Variable que va a contener el valor del dezplazamiento de bits que va a indicar la decision a tomar
+                int xAux;
+
+                for (int i = 0; i <= cantidadTotalRutasPosibles; i++)
+                {
+                    //Obtengo el primer valor de la matriz para a partir de ahi comenzar las posibles rutas
+                    //y comparar el resultado final
+                    sumaActual = matrizPiramidal[0][0];
+                    listaActualTomada = new List<string>();
+                    listaActualTomada.Add("#" + i.ToString() + " -> ");
+                    listaActualTomada.Add(matrizPiramidal[0][0].ToString());
+                    xAux = 0;
+                    for (int j = 0; j < matrizPiramidal.Length - 1; j++)
+                    {
+                        xAux = xAux + (i >> j & 1);
+                        sumaActual += matrizPiramidal[j + 1][xAux];
+                        //Guardo cada elemento de la ruta tomada para mostrar la ruta valida al final
+                        listaActualTomada.Add(matrizPiramidal[j + 1][xAux].ToString());
+                    }
+
+
+                    string rutaActual = string.Empty;
+                    for (int h = 0; h < listaActualTomada.Count(); h++)
+                    {
+                        if (h < listaActualTomada.Count() - 1)
+                        {
+                            rutaActual += listaActualTomada[h].ToString() + " - ";
+                        }
+                        else
+                        {
+                            rutaActual += listaActualTomada[h].ToString();
+                        }
+                    }
+
+                    historialRutas.AppendLine(rutaActual);
+
+                    if (sumaActual > sumaRutaMaxima)
+                    {
+                        sumaRutaMaxima = sumaActual;
+                        listaRutaFinalTomada = listaActualTomada;
+                    }
+                }
+                string ruta = string.Empty;
+                for (int i = 0; i < listaRutaFinalTomada.Count(); i++)
+                {
+                    if (i < listaRutaFinalTomada.Count() - 1)
+                    {
+                        ruta += listaRutaFinalTomada[i].ToString() + " - ";
+                    }
+                    else
+                    {
+                        ruta += listaRutaFinalTomada[i].ToString();
+                    }
+
+                }
+                //================================
+                txtRutaFinalTomada.Text = ruta;
+                txtSumaMaxima.Text = sumaRutaMaxima.ToString();
+                richTextBox2.Text = historialRutas.ToString();
+                txtCantidadTotalRutas.Text = cantidadTotalRutasPosibles.ToString();
+
+            }
+            else //Solución mas rapida para cuando es una piramide muy grande, y se utiliza un algoritmo mas eficiente.
+            {
+                int cantidadNivelesPiramide = this.matrizValores.Length;
+                txtRutaFinalTomada.Text = "Ruta única -> ";
+                historialRutas.AppendLine("Historial de sumas acumuladas:");
+                //Se comienza a iterar de abajo hacia arriba para ir acortando los niveles de la piramide 
+                //Con la suma mayor de los dos hijos con el padre inmediato superior.
+                for (int x = cantidadNivelesPiramide - 2; x >= 0; x--)
+                {
+                    for (int y = 0; y <= x; y++)
+                    {
+                        this.matrizValores[x][y] += Math.Max(this.matrizValores[x + 1][y], this.matrizValores[x + 1][y + 1]);
+                        
+                        listaActualTomada = new List<string>();
+                        listaActualTomada.Add(Math.Max(copiaMatrizOriginal[x + 1][y], copiaMatrizOriginal[x + 1][y + 1]).ToString() + " - ");
+
+                        copiaListaActualTomada = new List<string>();
+                        copiaListaActualTomada.Add(Math.Max(this.matrizValores[x + 1][y], this.matrizValores[x + 1][y + 1]).ToString() + " - ");
+                    }
+                    string rutaActual = string.Empty;
+                    string historialSumaRutaActual = string.Empty;
+
+                    for (int h = 0; h < listaActualTomada.Count(); h++)
+                    {
+                        if (h < listaActualTomada.Count() - 1)
+                        {
+                            rutaActual += listaActualTomada[h].ToString() + " - ";
+                            historialSumaRutaActual += copiaListaActualTomada[h].ToString() + " - ";
+                        }
+                        else
+                        {
+                            rutaActual += listaActualTomada[h].ToString();
+                            historialSumaRutaActual += copiaListaActualTomada[h].ToString();
+                        }
+                    }
+                    txtRutaFinalTomada.Text += rutaActual;
+                    historialRutas.AppendLine(historialSumaRutaActual);
+                }
+
+                //================================               
+                txtSumaMaxima.Text = this.matrizValores[0][0].ToString();
+                richTextBox2.Text = historialRutas.ToString();
+                txtCantidadTotalRutas.Text = Math.Pow(2, matrizPiramidal.Length - 1).ToString();
+            }
+        }
+    }
+}
